cd8911898aa9f6253f4bd4087a987a42aa191cd0
Doheera-kosi <kupourevans99@gmail.com>
2022-10-04 12:43:44 +0000

    Update project files

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -13,6 +16,13 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
+    optimization: {
+      runtimeChunk: 'single',
+    },
+      devServer: {
+    static: './dist',
+  },
   },
   module: {
     rules: [
